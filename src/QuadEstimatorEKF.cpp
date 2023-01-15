ed4181ea0a2548b16dcb882a3672a06a6da6b27f
Rutvik Patel <rutvik.p4t3l@gmail.com>
2023-01-15 14:26:51 -0800

    complete gps update

--- src/QuadEstimatorEKF.cpp
+++ src/QuadEstimatorEKF.cpp
@@ -295,9 +295,23 @@ void QuadEstimatorEKF::UpdateFromGPS(V3F pos, V3F vel)
   // Hints: 
   //  - The GPS measurement covariance is available in member variable R_GPS
   //  - this is a very simple update
-  ////////////////////////////// BEGIN STUDENT CODE ///////////////////////////
+  ////////////////////////////// BEGIN GPS Update ///////////////////////////
 
-  /////////////////////////////// END STUDENT CODE ////////////////////////////
+  zFromX(0) = ekfState(0);
+  zFromX(1) = ekfState(1);
+  zFromX(2) = ekfState(2);
+  zFromX(3) = ekfState(3);
+  zFromX(4) = ekfState(4);
+  zFromX(5) = ekfState(5);
+
+  hPrime(0,0) = 1;
+  hPrime(1,1) = 1;
+  hPrime(2,2) = 1;
+  hPrime(3,3) = 1;
+  hPrime(4,4) = 1;
+  hPrime(5,5) = 1;
+
+  /////////////////////////////// END Gps Update ////////////////////////////
 
   Update(z, hPrime, R_GPS, zFromX);
 }
